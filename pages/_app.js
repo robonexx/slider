f711840a7644e099b31b32c8659d3471bd312ada
robonexx <robertwagar@gmail.com>
2022-03-16 11:17:16 +0100

    React slider / next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/resets.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import styles from '../styles/Home.module.css'
+import styled from '../styles/Slider.module.css'
+import { Carousel } from 'react-responsive-carousel'
+
+export default function Home(props) {
+ 
+  console.log(props.posts)
+
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -13,57 +21,65 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         image slider
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <Carousel infiniteLoop autoPlay className={styled.wrapper}>
+              <div className="image">
+            <img src="/keyboard.jpg" alt="mobile" className={styled.image} />
+            </div>
+              <div className="image">
+            <img src="/keyboard2.jpg" alt="mobile" className={styled.image} />
+            </div>
+              <div className="image">
+            <img src="/controller.jpg" alt="mobile" className={styled.image} />
+            </div>
+              <div className="image">
+            <img src="/stargazed.jpg" alt="mobile" className={styled.image} />
+            </div>
+        </Carousel>
+        
+        <div>
+          <h1>TEST SLIDER COMPONENT with api fetch</h1>
+          <Carousel className={styled.wrapperDos}>
+            {props.posts.map(post => (
+              <div className="image" key={post.id}>
+                <h1 className={styled.title}>{post.name}</h1>
+               <img src={post.image} alt="mobile" className={styled.image} />
+               </div>
+            ))}
+          </Carousel>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            FOOTER
           </span>
-        </a>
       </footer>
     </div>
   )
 }
+/* 
+export const Posts = props => (
+  <div>
+     {props.posts.map(post => (
+       
+            <div className='image' key={post.id}>
+              <img src={post.image} alt='mobile' className={styles.image} />
+              <h1 className={styles.name}>{post.name}</h1>
+            </div>
+        ))}
+  </div>
+) */
+
+
+Home.getInitialProps = async function () {
+  const res = await fetch(
+    'https://my-json-server.typicode.com/robonexx/db/posts'
+  );
+  const data = await res.json();
+  return {
+    posts: data,
+  };
+};
